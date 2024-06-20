b7d375d23ed55eeb072f8d74de6a83f53014a141
akauzinha47 <kau.vmenezes@gmail.com>
2024-06-20 14:48:17 -0300

    refact: main and .vscode

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <windows.h>
 
-#define WIDTH 20
+#define WIDTH 40
 #define HEIGHT 20
 #define SPACE_BTW 5
 
@@ -22,8 +22,9 @@ int main()
     // array[HEIGHT - 3][8] = 9;
 
     int y = 8;
-    int x1 = WIDTH - 3;
+    int x1 = WIDTH - 4;
     int x2 = 0;
+    int x3 = 0;
 
     for (int i = (HEIGHT - y); i < (HEIGHT); i++)
     {
@@ -43,6 +44,9 @@ int main()
     while (1)
     {
 
+        sleep(1);
+        system("cls");
+
         if (x1 == 0)
         {
             for (int i = (HEIGHT - y); i < (HEIGHT); i++)
@@ -59,97 +63,139 @@ int main()
                 array[i][x1 + 2] = 0;
             }
 
-            x1 = WIDTH - 3;
+            x1 = WIDTH - 4;
         }
 
 
 
-        for (x1; x1 > 0; x1--)
+        if (x2 == 0)
         {
-            if (x2 == 0)
-            {
-                printf("X2 = 0 MANO\n");
-                for (int i = (HEIGHT - y); i < (HEIGHT); i++)
-                {
-                    array[i][x2] = 0;
-                    array[i][x2 + 1] = 0;
-                    array[i][x2 + 2] = 0;
-                }
-
-                for (int i = (HEIGHT - y) - SPACE_BTW; i > 0; i--)
-                {
-                    array[i][x2] = 0;
-                    array[i][x2 + 1] = 0;
-                    array[i][x2 + 2] = 0;
-                }
-
-            }
-            if (x1 == WIDTH / 2)
-            {
-                x2 = WIDTH - 3;
-            }
-
-            if (x2 != 0)
-            {
-                for (int i = (HEIGHT - y); i < (HEIGHT); i++)
-                {
-
-                    array[i][x2 - 1] = 1;
-                    array[i][x2 + 2] = 0;
-                }
-
-                for (int i = (HEIGHT - y) - SPACE_BTW; i > 0; i--)
-                {
-                    array[i][x2 - 1] = 1;
-                    array[i][x2 + 2] = 0;
-                }
-
-                x2--;
-            }
-
-            sleep(1);
-            system("cls");
-
             for (int i = (HEIGHT - y); i < (HEIGHT); i++)
             {
-
-                array[i][x1 - 1] = 1;
-                array[i][x1 + 2] = 0;
+                array[i][x2] = 0;
+                array[i][x2 + 1] = 0;
+                array[i][x2 + 2] = 0;
             }
 
             for (int i = (HEIGHT - y) - SPACE_BTW; i > 0; i--)
             {
-                array[i][x1 - 1] = 1;
-                array[i][x1 + 2] = 0;
+                array[i][x2] = 0;
+                array[i][x2 + 1] = 0;
+                array[i][x2 + 2] = 0;
             }
 
-            printf("%d - %d\n", x1, x2);
-            for (int i = 0; i < WIDTH; i++)
+        }
+
+        if (x3 == 0)
+        {
+            for (int i = (HEIGHT - y); i < (HEIGHT); i++)
             {
-                printf("[]");
+                array[i][x3] = 0;
+                array[i][x3 + 1] = 0;
+                array[i][x3 + 2] = 0;
             }
-            printf("\n");
 
-            for (int x1 = 0; x1 < HEIGHT; x1++)
+            for (int i = (HEIGHT - y) - SPACE_BTW; i > 0; i--)
             {
-                for (int j = 0; j < WIDTH; j++)
-                {
-                    if (array[x1][j] == 0)
-                    {
-                        printf("  ");
-                    } else if (array[x1][j] == 9){
-                        printf(" X");
-                    } else {
-                        printf("[]");
-
-                    }
-                }
-                
-            printf("\n");
+                array[i][x3] = 0;
+                array[i][x3 + 1] = 0;
+                array[i][x3 + 2] = 0;
             }
-    
+
+        }
+        if (x1 == WIDTH - 16)
+        {
+            x2 = WIDTH - 4;
+        }
+
+        if (x2 == WIDTH - 16)
+        {
+            x3 = WIDTH - 4;
+        }
+
+        if (x2 != 0)
+        {
+            for (int i = (HEIGHT - y); i < (HEIGHT); i++)
+            {
+
+                array[i][x2 - 1] = 1;
+                array[i][x2 + 2] = 0;
+            }
+
+            for (int i = (HEIGHT - y) - SPACE_BTW; i > 0; i--)
+            {
+                array[i][x2 - 1] = 1;
+                array[i][x2 + 2] = 0;
+            }
+
+            x2--;
+        }
+
+            if (x3 != 0)
+        {
+            for (int i = (HEIGHT - y); i < (HEIGHT); i++)
+            {
+
+                array[i][x3 - 1] = 1;
+                array[i][x3 + 2] = 0;
+            }
+
+            for (int i = (HEIGHT - y) - SPACE_BTW; i > 0; i--)
+            {
+                array[i][x3 - 1] = 1;
+                array[i][x3 + 2] = 0;
+            }
+
+            x3--;
         }
         
+
+        for (int i = (HEIGHT - y); i < (HEIGHT); i++)
+        {
+
+            array[i][x1 - 1] = 1;
+            array[i][x1 + 2] = 0;
+        }
+
+        for (int i = (HEIGHT - y) - SPACE_BTW; i > 0; i--)
+        {
+            array[i][x1 - 1] = 1;
+            array[i][x1 + 2] = 0;
+        }
+
+        x1--;
+
+        printf("%d - %d - %d\n", x1, x2, x3);
+        for (int i = 0; i < WIDTH; i++)
+        {
+            printf("[]");
+        }
+        printf("\n");
+
+        for (int i = 0; i < HEIGHT; i++)
+        {
+            for (int j = 0; j < WIDTH; j++)
+            {
+                if (array[i][j] == 0)
+                {
+                    printf("  ");
+                } else if (array[i][j] == 9){
+                    printf(" X");
+                } else {
+                    printf("[]");
+
+                }
+            }
+
+            // for (int j = 0; j < WIDTH; j++)
+            // {
+            //     printf("%d", array[i][j]);
+            // }
+            
+        printf("\n");     
+        }
+    
     }
+  
 }
 
